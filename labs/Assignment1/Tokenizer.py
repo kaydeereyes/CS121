819587fed83a5f6aca91e3e50d27bc9c490d1c3d
kaydeereyes <reyeskf@uci.edu>
2026-01-20 15:45:06 -0800

    Updated Tokenizer.py & included test texts

--- labs/Assignment1/Tokenizer.py
+++ labs/Assignment1/Tokenizer.py
@@ -6,19 +6,33 @@ class Token:
     def __init__(self, type, value):
         self.type = type
         self.value = value
-        self.frequency = 0
+        self.count = 0
 
-    def increment_count(self):
-        self.frequency += 1
+    def increment_count(self): # Do I Need This if i just use the count method?
+        self.count += 1
 
-    def get_frequency(self):
-        return self.frequency
+    def set_count(self, n):
+        self.count = n
+
+    def get_count(self):
+        return self.count
 
 def tokenize(TextFilePath) -> list<Token>:
     tokenList = list()
 
     with open(TextFilePath, 'r') as file:
-        content = f.read() 
+        content = f.read(100) #Reads the first 100 chars in case of large files
+        contentLower = content.lower()
+        preprocessedWords = contentLower.split()
+        filteredWords = [word for word in preprocessedWords if len(word) >= 3]
+    
+    tokenSet = set(filteredWords)
+    
+    
+
+    
+
+
     
 
 
