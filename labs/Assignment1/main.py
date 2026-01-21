a27228177e401f73358d7f3d78e7b9a8951446fd
kaydeereyes <reyeskf@uci.edu>
2026-01-21 21:01:41 +0000

    Completed Part A: Tokenizer. computeWordFrequencies & printTokens

--- labs/Assignment1/main.py
+++ labs/Assignment1/main.py
@@ -2,7 +2,7 @@ import tokenizer
 
 def main():
     path = input('Please input file path: ')
-    tokenList = tokenizer.tokenize(path)
+    tokenizer.runTokenizer(path)
 
 if __name__ == '__main__':
     main()
--- labs/Assignment1/tokenizer.py
+++ labs/Assignment1/tokenizer.py
@@ -1,12 +1,16 @@
-import re
-import sys
-import random
+
 import typing
 
 class Token:
-    def __init__(self, value):
-        self.value = value
+    def __init__(self, word):
+        self.word = word
         self.count = 0
+
+    def __eq__(self, other):
+        return isinstance(other, Token) and self.word == other.word
+
+    def __hash__(self):
+        return hash(self.word)
 
     def increment_count(self): # Do I Need This if i just use the count method?
         self.count += 1
@@ -17,6 +21,9 @@ class Token:
     def get_count(self):
         return self.count
 
+    def value(self):
+        return self.word
+
 #Should I create a method that just runs all 3? Tokenize, Count, and Print?
 
 def open_file(path) -> list:
@@ -24,7 +31,7 @@ def open_file(path) -> list:
         content = f.read(100) #Reads the first 100 chars in case of large files
         contentLower = content.lower()
         preprocessedWords = contentLower.split()
-        filteredWords = [word for word in preprocessedWords if len(word) >= 3]
+        filteredWords = [word for word in preprocessedWords if len(word) > 3]
     return filteredWords
 
 def tokenize(TextFilePath) -> list[Token]:
@@ -43,7 +50,20 @@ def computeWordFrequencies(tokenList: list[Token]) -> dict[Token, int]:
     for token in tokenList:
         tokenCount[token] = tokenCount.get(token, 0) + 1
 
-    return tokenCount
+    sortedCount = dict(sorted(tokenCount.items(), 
+                              key=lambda item: item[1], reverse = True))
+
+    return sortedCount
+
+def printTokens(tokens: dict[Token, int]) -> None:
+    for token, count in tokens.items():
+        print(f'{token.value()} {count}\n')
+
+def runTokenizer(TextFilePath) -> None:
+    tokenList = tokenize(TextFilePath)
+    tokenCount = computeWordFrequencies(tokenList)
+    printTokens(tokenCount)
+        
 
 
     
